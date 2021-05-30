68faa6de2892dd82805a44aa409d8d9a9194b7d8
stacysorey <stacysorey@gmail.com>
2021-05-30 01:55:07 -0400

    commit tree 1f48ec5c2279bca02f9bf097e4aca3c8a4580729

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+// no longer has DOM node 'main#main
+// has a 'newHeader' variable that points to node 'h1#victory
+// has a 'newHeader' variable that points to node 'h1#victory
+// has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside
+
+const testVar = "hi";
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
--- test/indexTest.js
+++ test/indexTest.js
@@ -15,6 +15,6 @@ describe("index.html", () => {
     it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
       expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
     });
-
+ 
   });
 })
